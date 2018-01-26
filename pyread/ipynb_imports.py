bee9f82516ba776602073ea27410d87f32de0bbc
magnucb <magnucb@astro.uio.no>
2018-01-26 18:16:15 +0100

    fof initial bugshooting

---
+++ pyread/ipynb_imports.py
@@ -0,0 +1,48 @@
+import sys, os, glob, time
+import numpy as N
+import subprocess as sp
+import pylab as pl
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import rc
+rc('font',**{'family':'serif'})
+time_init_start = time.time()
+
+# ==============================================
+# Read.& proc. toolkit for data sets' structure.
+# ==============================================
+from read_usertools import UserTools
+
+# ==============================================
+# Read.& proc. data sets' visualisation tools.
+# ==============================================
+from read_plotdoc import Plotter
+
+# ==============================================
+# Reading toolkit for data sets' structure.
+# ==============================================
+from read_misctools import MiscTools
+
+# ==============================================
+# File interpreters for read.py's interface.
+# ==============================================
+from read_sifters import Sifters
+
+# ==============================================
+# Reading procedure for data sets' structure.
+# ==============================================
+from read_procedures import readProcedures
+
+# ==============================================
+# Argument interpreter for read.py's interface.
+# ==============================================
+from read_args import readArgs
+
+# ==============================================
+# Indra data sets' reading MO.
+# ==============================================
+from read import readDo
+
+time_init_end = time.time()
+
+print "Initialization time: {0:.2f} seconds".format((time_init_end - time_init_start))
