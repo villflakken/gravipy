755d38cb94fac7f9f6903d1a731a5ba17fe49a37
magnucb <magnucb@astro.uio.no>
2018-02-11 18:19:26 +0100

    lessprint curiosity

--- pyread/read_misctools.py
+++ pyread/read_misctools.py
@@ -197,8 +197,8 @@ class MiscTools(object):
     def itertextPrinter(self, itertext, i, iterLen, modifier):
         " Less spam in terminal window "
         print 
-        print "lessprint                    :", self.lessprint
-        print "i % (self.printNth*modifier) :", i % (self.printNth*modifier)
+        print "lessprint                         :", self.lessprint
+        print "i % (self.printNth*modifier) == 0 :", i % (self.printNth*modifier) == 0
         if self.boolcheck(self.lessprint) == False:
             # No output reduction:
             print itertext
