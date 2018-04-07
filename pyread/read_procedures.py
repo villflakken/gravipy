4d6a5b4a3fa2aeca9bdcb58ef62b7a6550226430
magnucb <magnucb@astro.uio.no>
2018-04-07 05:18:20 +0200

    FSU01a.roughTesting AllSnaps-alg seems to work

--- pyread/read_procedures.py
+++ pyread/read_procedures.py
@@ -190,7 +190,7 @@ class readProcedures(Sifters, MiscTools, UserTools, AutoTools, Plotter):
         fofIDs    , groupLen, groupOffset = self.fof_ids_sifter(gids_name, groupLen, groupOffset)
         print "    TotNgroups = ({0:>10d})".format(TotNgroups)
 
-        print "   . Finished reading '"+str(self.what)
+        print "   . Finished reading 'FoF'"
         return fofIDs, TotNgroups, groupLen, groupOffset
 
 
@@ -213,7 +213,7 @@ class readProcedures(Sifters, MiscTools, UserTools, AutoTools, Plotter):
         subIDs  = self.subh_idsifter(sids_name, TotNsubs, NTask)
         print "    TotNsubs   = ({0:>10d})".format(TotNsubs)
                 
-        print "   . Finished reading '"+str(self.what)
+        print "   . Finished reading 'SubHalo"
         return subIDs, TotNsubs, catalog
 
 
@@ -242,7 +242,7 @@ class readProcedures(Sifters, MiscTools, UserTools, AutoTools, Plotter):
             sys.exit(ori_open_error_str)
 
 
-        print "   . Finished reading '"+str(self.what)
+        print "   . Finished reading 'Origami'"
         return tags, Npart[0]
 
 
