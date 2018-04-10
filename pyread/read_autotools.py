0acfd86dbbfeb67f96f536a7bc93f6d57d59e3b2
magnucb <magnucb@astro.uio.no>
2018-04-10 03:49:09 +0200

    FSU01a.roughTesting AllSnaps-alg seems to work

--- pyread/read_autotools.py
+++ pyread/read_autotools.py
@@ -190,7 +190,7 @@ class AutoTools(object):
         for si in self.sIndex:
             sn = self.subfolder_set[si]
             nOtags[si] = N.sum( # Sum(bools(type)) => N(type)
-                self.dataAlldict['origami'][self.iString][sn] == oTag_dict[otype]
+                self.dataAlldict['origami'][self.iString][sn][0] == oTag_dict[otype]
             )
             continue
 
