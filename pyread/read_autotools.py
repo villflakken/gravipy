2114ae94c023830b05f856ecb9e236d8a24c72c0
magnucb <magnucb@astro.uio.no>
2018-09-04 18:39:51 +0200

    FSU01a.playing:moving all exp. to Jupyter

--- pyread/read_autotools.py
+++ pyread/read_autotools.py
@@ -118,12 +118,6 @@ class AutoTools(object):
         #| origamitag_array, N_of_particles
 
 
-
-        " I should have done this ages ago!..."
-        # ...and now it is finally being done:
-        sys.exit("Handing over control to Jupyter from this point onwards!")
-
-
         # --- Put processing stuff here! ------------------------
         """
         " Sums of FoF tags and Subhalo tags "
